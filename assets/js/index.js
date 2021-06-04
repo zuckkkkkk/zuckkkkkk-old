cd7b967fae456db7aadf8d0feb6298eb1fbde6af
Mattia Zucchini <lupozeta@gmail.com>
2021-06-04 16:52:11 +0200

    ,,

--- assets/js/index.js
+++ assets/js/index.js
@@ -218,3 +218,11 @@ const render = () => {
 }
 render()
 
+/*Carousel Old New*/
+$("#slider").on("input change", (e)=>{
+  const sliderPos = e.target.value;
+  // Update the width of the foreground image
+  $('.foreground-img').css('width', `${sliderPos}% `)
+  // Update the position of the slider button
+  $('.slider-button').css('left', `calc(${sliderPos}% - 18px)`)
+}); 
