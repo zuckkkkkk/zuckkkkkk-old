0a9a7c78d02b53bcd18bd2f510ee53fc30a85278
Mattia Zucchini <lupozeta@gmail.com>
2021-10-08 17:38:45 +0200

    Service & bit of bollette update

---
+++ assets/js/services.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    particlesJS.load('hero', 'assets/json/particlesjs-config-services.json', function() {
+        console.log('callback - particles.js config loaded');
+    });
+});
