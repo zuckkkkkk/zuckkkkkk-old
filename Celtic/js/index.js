f0430aca15423390ec248bb2f5a95b724f04ea26
Mattia Zucchini <lupozeta@gmail.com>
2021-11-03 09:26:57 +0100

    Update000

---
+++ Celtic/js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#itToEn").click(function() {
+        window.location = "../en/index.html"
+      });
+      $("#EnToIt").click(function() {
+        window.location = "../it/index.html"
+      });
+});
