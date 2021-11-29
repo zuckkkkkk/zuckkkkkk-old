3a53fb6a8b9c5daa45f0daa940f69e46c48f1a5b
lupozeta <lupozeta@gmail.com>
2021-11-29 11:00:07 +0100

    Modifiche

--- Celtic/js/index.js
+++ Celtic/js/index.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
+      //Cambio lingua
     $("#itToEn").click(function() {
         window.location = "../en/index.html"
       });
       $("#EnToIt").click(function() {
         window.location = "../it/index.html"
       });
+      //Check ora per Happy Hour
+      const date = new Date();
+      if (date.getHours() < 20 && date.getHours() > 10 ){
+        $("#happyhour").show();
+      }else{
+        $("#happyhour").hide();
+      }
 });
