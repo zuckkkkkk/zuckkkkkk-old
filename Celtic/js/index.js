f2b22893d55f84d3f2a93fc4093554a8ff7755b7
lupozeta <lupozeta@gmail.com>
2021-12-01 21:38:06 +0100

    Update lato tecnico

--- Celtic/js/index.js
+++ Celtic/js/index.js
@@ -14,3 +14,21 @@ $(document).ready(function() {
         $("#happyhour").hide();
       }
 });
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    $("#goBackbtn").show();
+    console
+  } else {
+    $("#goBackbtn").hide();
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
